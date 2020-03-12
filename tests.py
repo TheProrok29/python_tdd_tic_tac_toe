84b278c0aad235ef51e0cd80ee15fa0eb4bbee09
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-12 08:55:32 +0100

    Add test to check error when field is occupied.

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+
 from tic import TicTacToe
 
 
@@ -12,6 +13,12 @@ class TicTacToeSpec(unittest.TestCase):
         tic_tac_toe = TicTacToe()
         with self.assertRaises(Exception):
             tic_tac_toe.play(2, 5)
+
+    def test_when_field_is_occupied_then_error(self):
+        tic_tac_toe = TicTacToe()
+        tic_tac_toe.play(2, 2)
+        with self.assertRaises(Exception):
+            tic_tac_toe.play(2, 2)
 
 
 if __name__ == '__main__':
