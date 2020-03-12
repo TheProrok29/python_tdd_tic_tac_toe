165119a22dccba1734c1bdab9dbbfdebd529cbd6
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-12 08:52:32 +0100

    Add minimum code to pass y outside board test.

--- tic.py
+++ tic.py
@@ -3,3 +3,5 @@ class TicTacToe:
     def play(self, x: int, y: int) -> None:
         if x < 1 or x > 3:
             raise Exception('X outside the board')
+        elif y < 1 or y > 3:
+            raise Exception('Y outside the board')
