567bf910c0d8c13a5e3fa92065c2886d10879e8b
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-12 08:28:31 +0100

    Add Travis config file

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from source import TicTacToe
+
+
+class TicTacToeSpec(unittest.TestCase):
+    def test_when_x_outside_board_then_error(self):
+        tic_tac_toe = TicTacToe()
+        with self.assertRaises(Exception): tic_tac_toe.play(5, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
