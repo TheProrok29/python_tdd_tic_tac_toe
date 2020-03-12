aadde9d1e56d75bcaa7e0b34719d45cffb62eac8
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-12 09:00:41 +0100

    Add board to TicTacToe constructor and check occupied field in play() to pass the test.

--- tic.py
+++ tic.py
@@ -1,7 +1,15 @@
 class TicTacToe:
+    def __init__(self):
+        self.board = [['O', 'O', 'O'],
+                      ['O', 'O', 'O'],
+                      ['O', 'O', 'O']]
 
     def play(self, x: int, y: int) -> None:
         if x < 1 or x > 3:
             raise Exception('X outside the board')
         elif y < 1 or y > 3:
             raise Exception('Y outside the board')
+        if self.board[x - 1][y - 1] == 'O':
+            self.board[x - 1][y - 1] = 'X'
+        else:
+            raise Exception('Field is occupied')
