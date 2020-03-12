8dcc4d1e2cee28a29ad291d84540a2b6fb1ef306
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-12 09:43:27 +0100

    Add test to check first turn when player is X.

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class TicTacToeSpec(unittest.TestCase):
     def setUp(self) -> None:
         self.tic_tac_toe = TicTacToe()
 
-    def testWhen_x_outside_board_then_error(self):
+    def test_when_x_outside_board_then_error(self):
         with self.assertRaises(Exception):
             self.tic_tac_toe.play(5, 2)
 
@@ -19,6 +19,9 @@ class TicTacToeSpec(unittest.TestCase):
         self.tic_tac_toe.play(2, 2)
         with self.assertRaises(Exception):
             self.tic_tac_toe.play(2, 2)
+
+    def test_first_turn_when_next_player_is_x(self):
+        self.assertEqual('X', self.tic_tac_toe.next_player())
 
 
 if __name__ == '__main__':
