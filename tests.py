801d9680f444fd3c1cd130fcaeda3210e09e0bbe
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-12 09:23:34 +0100

    Add create TicTacToe instance in test setUp().

--- tests.py
+++ tests.py
@@ -4,21 +4,21 @@ from tic import TicTacToe
 
 
 class TicTacToeSpec(unittest.TestCase):
-    def test_when_x_outside_board_then_error(self):
-        tic_tac_toe = TicTacToe()
+    def setUp(self) -> None:
+        self.tic_tac_toe = TicTacToe()
+
+    def testWhen_x_outside_board_then_error(self):
         with self.assertRaises(Exception):
-            tic_tac_toe.play(5, 2)
+            self.tic_tac_toe.play(5, 2)
 
     def test_when_y_outside_board_then_error(self):
-        tic_tac_toe = TicTacToe()
         with self.assertRaises(Exception):
-            tic_tac_toe.play(2, 5)
+            self.tic_tac_toe.play(2, 5)
 
     def test_when_field_is_occupied_then_error(self):
-        tic_tac_toe = TicTacToe()
-        tic_tac_toe.play(2, 2)
+        self.tic_tac_toe.play(2, 2)
         with self.assertRaises(Exception):
-            tic_tac_toe.play(2, 2)
+            self.tic_tac_toe.play(2, 2)
 
 
 if __name__ == '__main__':
